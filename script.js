30f747c76c792f70da194e821932fbca50f9e66e
Myrithick <m.myrithick111@gmail.com>
2025-04-23 15:54:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('appear');
+    observer.unobserve(entry.target);
+  });
+}, { threshold: 0.1 });
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+  
